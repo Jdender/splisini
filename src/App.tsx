c5f641781c128011f9aaa3e04e977aaa83d98ece
Jdender~ <jdenderplays@gmail.com>
2019-08-20 11:50:21 -0400

    Feat: Add temp useReducer in App to help test

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,33 @@
-import * as React from 'react';
+import { createElement, useReducer, Fragment } from 'react';
+import { Output } from './Output';
+import { Input } from './Input';
 
-export const App = () => (
-    <h1>Doot</h1>
-);
+export type Action = 
+    { type: 'push', value: string } |
+    { type: 'flush' };
+
+const reducer = (state: string[], action: Action) => {
+    switch (action.type) {
+
+        case 'push':
+            return [...state, action.value];
+
+        case 'flush':
+            return [];
+
+        default:
+            throw new Error();
+    }
+};
+
+export const App = () => {
+
+    const [buffer, dispatch] = useReducer(reducer, []);
+
+    return <div className="container">
+
+        <Output buffer={buffer}/>
+        <Input dispatch={dispatch}/>
+
+    </div>;
+};
